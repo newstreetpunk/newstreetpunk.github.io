ff682b669ff37ccc0b63f4dc61661bef0577a21b
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2019-03-28 19:03:01 +0500

    Add files via upload

--- evic/app/js/common.js
+++ evic/app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
+	
+	$('.waranty-block__item').each(function(i){
+		$(this).children('span').html(+ i + + '1');
+	});
 
-	// Custom JS
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
 
 });
