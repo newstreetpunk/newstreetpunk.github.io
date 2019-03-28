c47d93b8d59a3d56bd9596880b12512791410210
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2019-03-28 22:10:35 +0500

    Add files via upload

--- evic/app/js/common.js
+++ evic/app/js/common.js
@@ -7,5 +7,13 @@ $(function() {
 	$('.top').click(function() {
 		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 	});
+	$('.mobile-btn').click(function(){
+		$(this).toggleClass('active');
+		$('.evik-menu').toggleClass('active');
+	});
+	$('.evik-menu ul li a').click(function(){
+		$('.mobile-btn').removeClass('active');
+		$('.evik-menu').removeClass('active');
+	});
 
 });
